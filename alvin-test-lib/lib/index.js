74670ffd6c416d066df998b5a9192729f2a85ff6
chenqingxiangit config --global user.name chenqingxian <alvin95@163.com>
2022-10-21 15:04:04 +0800

    初始化脚手架

---
+++ alvin-test-lib/lib/index.js
@@ -0,0 +1,5 @@
+module.exports={
+    sun(a,b){
+        return a+b
+    }
+}
---
+++ alvin-test/bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const lib=require('alvin-test-lib')
+let sun =lib.sun(2,5)
+console.log('sum', sun)
+console.log('test-alvin111')
